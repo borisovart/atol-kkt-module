039f4594e6e2cd146c444a6d38e7e0c3564aa3f6
Artem Borisov <evolton@ya.ru>
2021-02-25 13:55:50 +0600

    Refactor

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  AtolKktModuleExample
-//
-
-import Foundation
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type AtolKktModuleType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { AtolKktModule } = NativeModules;
-
-export default AtolKktModule as AtolKktModuleType;
